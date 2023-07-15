d831c9aef933a2eeeeca79f604ba27fe02493100
Brightiya <bightiyahen@gmail.com>
2023-07-15 13:16:35 +0200

    New console.log added.
    Signed-off-by: Brightiya <bightiyahen@gmail.com>

--- sketch2.js
+++ sketch2.js
@@ -2,6 +2,6 @@
 
 function my_name(){
     console.log("My name is Bright Iyahen.")
-    console.log("I'm a computer scientist. I am curious to make it.")
+    console.log("I'm a computer scientist. I am curious to make it all.")
 }
 my_name()
